294d91853d1368f8b3fb3647d7771cc63b0ee905
DiV4 <51695411+DiV4@users.noreply.github.com>
2021-10-19 16:20:42 +0300

    optional adjectives

--- source/text_gen/text_gen.gd
+++ source/text_gen/text_gen.gd
@@ -1,12 +1,12 @@
 extends Node
 
-const articles = ["the", "my", "your", "our", "that", "this", "every", "one", "the only", "his", "her"]
+const articles = ["the", "the", "the", "the", "my", "your", "our", "that", "this", "every", "one", "the only", "his", "her"]
 
 const nouns = ["forest", "city", "place", "plant", "sky", "ground", "mountain", "vehicle", "computer", "man", "woman", "person", "animal", "road", "device", "software", "family", "school", "bed", "mouse", "keyboard", "bicycle", "drink", "outfit", "decoration", "wall", "airport", "bridge", "road", "river", "beach", "sculpture", "instrument", "fruit", "window", "computer", "piece of paper", "photo", "artwork", "space", "task", "goal", "project", "lighthouse", "ocean", "planet"]
 
 const prepositions = ["under", "in front of", "on top of", "above", "behind", "near", "following", "inside", "outside", "besides", "unlike", "like", "beneath", "against", "into", "beyond", "considering", "without", "with", "towards"]
 
-const verbs = ["is", "was", "will be", "might be", "was never", "moves", "moved", "will move", "will end", "plays", "responded", "thinks", "is thinking", "waited", "waits", "enjoys", "hates", "illuminates", "faded", "transformed", "knows", "is working", "is calling", "stops", "continues", "speaks", "was speaking", "won", "lost", "is building", "destroyed", "is considering"]
+const verbs = ["is", "was", "will be", "might be", "was never", "moves", "moved", "will move", "plays", "responded", "thinks", "is thinking", "waited", "waits", "enjoys", "hates", "illuminates", "faded", "transformed", "knows", "is working", "is calling", "stops", "continues", "speaks", "was speaking", "won", "lost", "is building", "destroyed", "is considering"]
 
 const adjectives = ["happy", "sad", "moving", "static", "fast", "slow", "unbelievable", "infinte", "surprising", "mysterious", "glowing", "tired", "hard", "soft", "transparent", "long", "short", "excellent", "noisy", "silent", "rare", "common", "normal", "typical", "living", "clean", "dirty", "fancy", "scary", "helpless", "uncanny", "melodic", "silly", "kind", "brave", "nice", "old", "modern", "young", "sweet", "wet", "dry", "cold", "hot", "heavy", "industrial", "complex", "accurate", "awesome", "shiny", "cool", "fake", "unreal", "intelligent", "smart", "curious", "strange", "unique", "empty", "desaturated", "saturated", "blurry", "sharp", "warped"]
 
@@ -41,7 +41,11 @@ func _rand_punct():
 
 func _make_sentence():
 	var sentence = ""
-	sentence += _rand_art() + " " + _rand_adj() + " " + _rand_noun() + " " + _rand_verb()
-	sentence +=  " " + _rand_prep() + " " + _rand_art() + " " + _rand_adj() + " " + _rand_noun()
-	sentence += _rand_punct()
+	sentence += _rand_art() + " "
+	if globals.dream_rng.randf() > 0.5:
+		sentence += _rand_adj() + " "
+	sentence +=  _rand_noun() + " " + _rand_verb() + " " + _rand_prep() + " " + _rand_art() + " "
+	if globals.dream_rng.randf() > 0.5:
+		sentence += _rand_adj() + " "
+	sentence +=  _rand_noun() + _rand_punct()
 	return sentence
