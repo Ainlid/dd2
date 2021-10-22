aaceed5f166cc894707ae7ab3cba2d90ee9cf1da
DiV4 <51695411+DiV4@users.noreply.github.com>
2021-10-22 20:23:23 +0300

    human music

--- source/bgm_gen/bgm_gen.gd
+++ source/bgm_gen/bgm_gen.gd
@@ -1,49 +1,75 @@
 extends Node
 
-onready var player1 = $player1
-onready var player2 = $player2
-onready var player3 = $player3
-var offset1 = 0.0
-var offset2 = 0.0
-var offset3 = 0.0
-onready var timer = $timer
+onready var melody = $melody
+onready var bass = $bass
+
+onready var timer =$timer
+var tempo = 120.0
+
+var note_scale = []
+var transposition = 0.0
+
+var melody_note_seq = []
+var melody_play_seq = []
+var bass_note_seq = []
+var bass_play_seq = []
+
+var current_note = 0
 
 func _ready():
-	timer.wait_time = globals.dream_rng.randf_range(2.0, 4.0)
-	timer.start()
-	_set_players()
-	_play_loops()
+	_bgm_start()
 
-func _set_players():
-	#PLAYER1
-	var id1 = globals.dream_rng.randi()%globals.loops.size()
-	var loop1 = globals.loops[id1]
-	player1.stream = loop1
-	offset1 = globals.dream_rng.randf_range(0.0, loop1.get_length())
-	var rand_note1 = globals.dream_rng.randi_range(-6, 6)
-	player1.pitch_scale = pow(2.0, rand_note1 / 12.0)
-	#player1.pitch_scale = globals.dream_rng.randf_range(0.8, 1.2)
-	#PLAYER2
-	var id2 = globals.dream_rng.randi()%globals.loops.size()
-	var loop2 = globals.loops[id2]
-	player2.stream = loop2
-	offset2 = globals.dream_rng.randf_range(0.0, loop2.get_length())
-	var rand_note2 = globals.dream_rng.randi_range(-6, 6)
-	player2.pitch_scale = pow(2.0, rand_note2 / 12.0)
-	#player2.pitch_scale = globals.dream_rng.randf_range(0.8, 1.2)
-	#PLAYER3
-	var id3 = globals.dream_rng.randi()%globals.loops.size()
-	var loop3 = globals.loops[id3]
-	player3.stream = loop3
-	offset3 = globals.dream_rng.randf_range(0.0, loop3.get_length())
-	var rand_note3 = globals.dream_rng.randi_range(-6, 6)
-	player3.pitch_scale = pow(2.0, rand_note3 / 12.0)
-	#player3.pitch_scale = globals.dream_rng.randf_range(0.8, 1.2)
+func _bgm_start():
+	_set_sample()
+	_make_scale()
+	_sequence()
+	tempo = globals.dream_rng.randf_range(100.0, 180.0)
+	timer.wait_time = 60.0/tempo
+	timer.start()
+
+func _set_sample():
+	var m_id = globals.dream_rng.randi()%globals.samples.size()
+	melody.stream = globals.samples[m_id]
+	var b_id = globals.dream_rng.randi()%globals.samples.size()
+	bass.stream = globals.samples[b_id]
+
+func _make_scale():
+	if globals.dream_rng.randf() > 0.5:
+		#major scale
+		note_scale = [0, 2, 4, 5, 7, 9, 11]
+	else:
+		#minor scale
+		note_scale = [0, 2, 3, 5, 7, 8, 10]
+	transposition = globals.dream_rng.randi_range(0, 11)
+
+func _sequence():
+	for melody_notes in 16:
+		var note_id = globals.dream_rng.randi()%7
+		melody_note_seq.append(note_scale[note_id])
+	for melody_plays in 16:
+		melody_play_seq.append(globals.dream_rng.randf() > 0.5)
+	for bass_notes in 16:
+		var note_id = globals.dream_rng.randi()%7
+		bass_note_seq.append(note_scale[note_id])
+	for bass_plays in 16:
+		if bass_plays%4 == 1:
+			bass_play_seq.append(true)
+		else:
+			bass_play_seq.append(globals.dream_rng.randf() > 0.5 and bass_plays%2 == 1)
+
+func _note_to_pitch(note_value):
+	return pow(2.0, note_value/12.0)
 
 func _timeout():
-	_play_loops()
-
-func _play_loops():
-	player1.play(offset1)
-	player2.play(offset2)
-	player3.play(offset3)
+	#melody and bass
+	melody.pitch_scale = _note_to_pitch(melody_note_seq[current_note] + transposition)
+	if melody_play_seq[current_note]:
+		melody.play()
+	bass.pitch_scale = _note_to_pitch(bass_note_seq[current_note] + transposition - 12.0)
+	if bass_play_seq[current_note]:
+		bass.play()
+	#loop
+	if current_note < 15:
+		current_note += 1
+	else:
+		current_note = 0
